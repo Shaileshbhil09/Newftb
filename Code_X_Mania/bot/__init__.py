2b1c8c66a8a103ec1e3c05bf385a537908913ce3
Shaileshbhil09 <98157774+Shaileshbhil09@users.noreply.github.com>
2022-05-13 10:44:29 +0530

    Update __init__.py

--- Code_X_Mania/bot/__init__.py
+++ Code_X_Mania/bot/__init__.py
@@ -4,7 +4,7 @@ import pyromod.listen
 from ..vars import Var
 
 StreamBot = Client(
-    session_name='Web Streamer',
+    session_name='Code_X_Mania',
     api_id=Var.API_ID,
     api_hash=Var.API_HASH,
     bot_token=Var.BOT_TOKEN,
